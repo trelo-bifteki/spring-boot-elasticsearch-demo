347e4911b423d851c1711a2c431fce343f9f0450
Lampros Papadimitriou <lampros.papadimitriou@check24.de>
2018-10-08 17:00:00 +0200

    initial skeleton import

---
+++ src/main/java/com/trelobifteki/demo/elasticsearch/springbootelasticsearchdemo/SpringBootElasticsearchDemoApplication.java
@@ -0,0 +1,12 @@
+package com.trelobifteki.demo.elasticsearch.springbootelasticsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticsearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElasticsearchDemoApplication.class, args);
+	}
+}
